66b62bf0628fe6b0b165bd64d78cb87dba9950e7
Jesús Enrique Rascón <blograso@gmail.com>
2021-02-03 12:37:06 +0100

    subsetting done

--- src/generalordinal.py
+++ src/generalordinal.py
@@ -9,11 +9,28 @@ from sklearn.datasets import load_iris
 
 
 
-# subset splitting
+
 def split_subsets(data, target):
+    '''Define subsets for a given dataset. It figures out
+    how many factors there are in the target value and 
+    subsets the original dataset based on that, returning
+    the subsets.'''
+
+
     # figure out how many factors there are in the target value
     unique_target = np.unique(target)
     print(unique_target)
+
+    # create a list of subsets, for each factor except for
+    # the last one
+    subsets = []
+    for i in range(0, len(unique_target)-1):
+        indices = np.where(target == unique_target[i])
+        subsets.append(data[indices])
+
+    print(subsets)
+
+
 
 
 # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100)
