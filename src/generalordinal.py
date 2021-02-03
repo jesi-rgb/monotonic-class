3c05a41f3b93f05607d3a046b73c406959a5ba5c
Jesús Enrique Rascón <blograso@gmail.com>
2021-02-03 14:11:33 +0100

    correct bug with subsetting

--- src/generalordinal.py
+++ src/generalordinal.py
@@ -30,24 +30,25 @@ def split_and_train(data, target=None, model=None):
     subsets the original dataset based on that, returning
     the subsets.'''
 
-
     # figure out how many factors there are in the target value
     unique_target = np.unique(target)
-    print(unique_target)
 
     # create a list of subsets, for each factor except for
     # the last one
     subsets = []
     for i in range(0, len(unique_target)-1):
         # binarize each dataset and save it to a list
-        binary_target = np.where(target==unique_target[i], 0, 1)
+        binary_target = np.where(target<=unique_target[i], 0, 1)
         binary_subset = np.array(list(zip(data, binary_target)), dtype='object')
         subsets.append(binary_subset)
 
+    print(subsets)
+
+    # if no model is provided, we propose a Tree Classifier
     if(model is None):
         model = DecisionTreeClassifier(criterion = "entropy", random_state = 100, max_depth=3, min_samples_leaf=5)
 
-    
+    # for each subset we train a model and save it
     models = []
     for subset in subsets:
         X = list(zip(*subset))[0]
@@ -55,20 +56,38 @@ def split_and_train(data, target=None, model=None):
         fit_model = model.fit(X, Y)
         models.append(fit_model)
 
-    return models
+    # return the list of models trained in each binary subset
+    return dict(zip(unique_target, models))
+
+def predict_ensemble(data_points, named_models):
+    k = len(named_models)
+    # first model
+    # first_model = models[0].predict_proba(data_points)
+    # print(1-first_model[0][0])
+    names = named_models.keys
+    models = named_models.values
+
+    if(k==2):
+        pass
+        # predict_class_1 = 1-models[0].predict_proba(data_points)[:,0]
+        # predict_class_2 = models[1].predict_proba(data_points)[:,0]
+        # a = list(zip(predict_class_1, predict_class_2))
+
+
+
+    else:
+        pass
+        # predictions = [1-model.predict_proba(data_points)[:,0] for model in models]
+        # print(predictions)
 
 
 
 
-# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100)
-# clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100, max_depth=3, min_samples_leaf=5)
-# clf_entropy.fit(X_train, y_train)
-# probs = clf_entropy.predict_proba(X_test)
 
 
 if __name__ == "__main__":
     data = load_iris().data
     target = load_iris().target
 
-    split_and_train(data, target=target)
-    predict(data, models)
+    models = split_and_train(data, target=target)
+    predict_ensemble(data, models)
